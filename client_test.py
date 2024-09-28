06a549122781eae5085aa0b0306c1e321ab2a4a5
huishingchong <huishingchong@gmail.com>
2024-09-28 16:42:25 +0100

    Add unit tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_ask']['price'] + quote['top_bid']['price'])//2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_ask']['price'] + quote['top_bid']['price'])//2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_emptyQuote(self):
+    """Test if getDataPoint handles empty quotes correctly."""
+    quote = {}
+    with self.assertRaises(KeyError):
+        getDataPoint(quote)
+    
+  def test_getDataPoint_invalidDataTypes(self):
+    """Test if getDataPoint handles quotes with invalid data types correctly."""
+    quote = {'top_ask': {'price': 'invalid', 'size': 'invalid'}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 'invalid', 'size': 'invalid'}, 'id': '0.109974697771', 'stock': 'ABC'}
+    with self.assertRaises(TypeError):
+        getDataPoint(quote)
 
 
 
